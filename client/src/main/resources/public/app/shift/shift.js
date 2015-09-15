cb26a902f2bc6ac9c19a54113c2793b07eee0332
pervoliner <pervoliner@gmail.com>
2015-09-16 02:02:28 +1000

    initial shift controller

---
+++ client/src/main/resources/public/app/shift/shift.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('myApp.employee', ['ui.layout', 'ui.grid', "ui.grid.selection"])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/employee', {
+            templateUrl: 'app/employee/employee.html',
+            controller: 'EmployeeCtrl'
+        });
+    }])
+
+    .controller('EmployeeCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+        $scope.gridOptions = {};
+
+        $scope.gridOptions.onRegisterApi = function(gridApi) {
+            //set gridApi on scope
+            $scope.gridApi = gridApi;
+        };
+
+        $http.get('app/data/services.columns.json')
+            .success(function (data) {
+                $scope.gridOptions.columnDefs = data;
+            });
+
+        $http.get('app/data/services.json')
+            .success(function (data) {
+                $scope.gridOptions.data = data;
+                $timeout(function() {
+                    angular.forEach($scope.user.services, function(id){
+                        var values = jQuery.grep($scope.gridOptions.data,
+                            function(e){
+                                return e.id == id;
+                            });
+                        if (values.length > 0) {
+                            $scope.gridApi.selection.selectRow(values[0]);
+                        }
+                    })
+                });
+            });
+
+        $scope.user = {
+            firstName: "Andrey",
+            lastName: "Koyro",
+            services: [
+                "cutting",
+                "directGlueing",
+                "patch"
+            ]
+        }
+
+    }]);
